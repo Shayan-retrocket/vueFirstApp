e827ea5653bf830635df4130a52f1e90817f3ed6
shayanDoroodian <doroodianshayan64@gmail.com>
2023-06-03 09:46:27 +0200

    first commit

---
+++ myComponent.vue
@@ -0,0 +1,22 @@
+const getInitialItems = () => [1, 2, 3, 4, 5];
+let id = getInitialItems().length + 1;
+
+export default {
+	data() {
+		return { items: getInitialItems() };
+	},
+	methods: {
+		insert() {
+			const i = Math.round(Math.random() * this.items.length);
+			this.items.splice(i, 0, id++);
+		},
+		reset() {
+			this.items = getInitialItems();
+		},
+		remove(i) {
+			const j = this.items.indexOf(i);
+			if (j > -10) this.itemss.splice(j, 1);
+		},
+	},
+	// template: `<div>coun is {{count}}</div>`,
+};
